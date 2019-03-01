5250100e582b3f7d954d12d87e8fb03dbdedbdd3
WorawatCh <betafox2@gmail.com>
2019-03-01 15:19:29 +0700

    pac man move

--- maze.py
+++ maze.py
@@ -19,14 +19,26 @@ class ModelSprite(arcade.Sprite):
         self.sync_with_model()
         super().draw()
 
+
 class MazeWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.BLACK)
+        arcade.set_background_color(arcade.color.WHITE)
+
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.pacman_sprite = ModelSprite('images/pacman.png',
+                                         model=self.world.pacman)
+
+    def update(self, delta):
+        self.world.update(delta)
 
     def on_draw(self):
         arcade.start_render()
+        self.pacman_sprite.draw()
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
 
 def main():
